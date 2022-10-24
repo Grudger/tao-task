513157bfaafea17a904744fa2e781b9a5c44d91e
Grudger <aamir00@hotmail.com>
2022-10-25 03:36:29 +0500

    Added validation schema and methods

--- frontend/src/components/StudentTable.jsx
+++ frontend/src/components/StudentTable.jsx
@@ -1,53 +1,95 @@
-import {useEffect, useState} from "react";
-import {DataGrid, useGridApiRef} from '@mui/x-data-grid';
+import {useEffect, useRef, useState} from "react";
+import {DataGrid, GridCellEditStopReasons, useGridApiRef} from '@mui/x-data-grid';
 import axios from "axios";
 import {BASE_URL} from "../config/Constants";
-import {studentColumns, StudentsMapper} from "../config/DataMapper";
+import {
+    studentColumns,
+    StudentsMapper,
+    validateAge,
+    validateEmail,
+    validateName,
+    validatePhoneNumber
+} from "../config/DataMapper";
 import StudentModal from "./modal/StudentModal";
 import {updateStudent} from "../middleware/DataRequest";
 
 export default function StudentTable(props) {
 
-  const [students, setStudents] = useState([]);
+    const [students, setStudents] = useState([]);
+    const [error, setError] = useState(false);
 
-  const apiRef = useGridApiRef();
+    const validateFields = (field, value) => {
+        // to cater drop down menu's content
+        console.log("validating !")
+        let isValid = false;
 
-  const onEdit = ({field, id}, event) => {
-    const value = event.target.value ? event.target.value : event.target.textContent
-    console.log('updating field ' + field, value)
-    updateStudent({[field]: value}, id)
+        switch (field) {
+            case "email": {
+                isValid = validateEmail(value)
+                break
+            }
+            case "phone" : {
+                isValid = validatePhoneNumber(value)
+                break
+            }
+            case "studentName": {
+                isValid = validateName(value)
+                break
+            }
+            case "age" : {
+                isValid = validateAge(value)
+                break
+            }
+            default : {
+            }
 
-  }
+        }
+        if (isValid) {
+            setError(false)
+        } else {
+            setError(true)
+        }
+
+    }
+
+    const onCommit = ({field, value, id, reason}) => {
+        validateFields(field, value)
+        if(reason === GridCellEditStopReasons.enterKeyDown)
+            updateStudent({[field]: value}, id)
+    }
 
 
-  useEffect(() => {
-    fetchData()
-  }, [])
+    useEffect(() => {
+        fetchData()
+    }, [])
 
-  const fetchData = () => {
-    console.log('updating Data')
-    axios.get(BASE_URL + "/listAll").then(r => {
-      if (r) {
-        setStudents(StudentsMapper(r.data))
-      } else
-        console.log('response in progress')
-    })
-  }
+    const fetchData = () => {
+        console.log('updating Data')
+        axios.get(BASE_URL + "/listAll").then(r => {
+            if (r) {
+                setStudents(StudentsMapper(r.data))
+            } else
+                console.log('response in progress')
+        })
+    }
 
-  return (
-    <div style={{height: 300, width: '100%'}}>
-      <StudentModal open={false} refreshData={fetchData}/>
-
-      <DataGrid rows={students}
-                columns={studentColumns}
-                stickyHeader
-                editMode="cell"
-                apiRef={apiRef}
-                disableIgnoreModificationsIfProcessingProps
-                onCellEditStop={(p, e) => onEdit(p, e)}
-                experimentalFeatures={{newEditingApi: true}}/>
-    </div>
-  )
+    return (
+        <div style={{height: 300, width: '100%'}}>
+            <StudentModal open={false} refreshData={fetchData}/>
+            {
+                error && <h4 className={"invalid"}>Please input valid data</h4>
+            }
+            <DataGrid rows={students}
+                      columns={studentColumns}
+                      stickyHeader
+                      editMode="cell"
+                      disableIgnoreModificationsIfProcessingProps
+                      //onCellEditStop={(p, e) => onEditStop(p, e)}
+                      onCellEditCommit={onCommit}
+                      experimentalFeatures={{newEditingApi: true, preventCommitWhileValidating: false}}
+            />
+        </div>
+    )
 
 
 }
